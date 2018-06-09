011c5238b6d5362e09d4801e0e184f8e5fd09c34
bwroblew <bartlomiej.wroblewski@intel.com>
2018-06-09 16:09:56 +0200

    singleton GameMaster

--- src/main/java/GameMaster.java
+++ src/main/java/GameMaster.java
@@ -1,8 +1,32 @@
 public class GameMaster {
     GameState state;
+    private GameMaster instance = null;
+    public GameMaster getInstance(){
+        if (instance == null){
+            instance = new GameMaster();
+        }
+        return instance;
+    }
+    private GameMaster(){};
+    void update(int index, String message) throws IndexOutOfBoundsException{
+        switch(message){
+            case "moveUp":
 
-    void update(){
-        
+                break;
+            case "moveDown":
+
+                break;
+            case "moveLeft":
+
+                break;
+            case "moveRight":
+
+                break;
+            case "shoot":
+
+                break;
+
+        }
     }
 
 }
